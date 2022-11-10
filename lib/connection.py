b08d19a1b60f49937b655918f91bf0457ab93420
Rozan Fadhil Al Hafidz <rozanfadhilalahfidz@gmail.com>
2022-11-10 08:51:21 +0700

    feat: update connection.py

--- lib/connection.py
+++ lib/connection.py
@@ -4,16 +4,23 @@ from .segment import Segment
 class Connection:
     def __init__(self, ip : str, port : int):
         # Init UDP socket
-        pass
+        self.ip  = ip
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.ip, self.port))
 
     def send_data(self, msg : Segment, dest : ("ip", "port")):
         # Send single segment into destination
-        pass
+        self.socket.sendto(msg.get_bytes(), dest)
 
     def listen_single_segment(self) -> Segment:
         # Listen single UDP datagram within timeout and convert into segment
-        pass
+        data, addr = self.socket.recvfrom(1024)
+        segment = Segment()
+        segment.set_from_bytes(data)
+        segment_checksum = segment.valid_checksum()
+        return addr, segment, segment_checksum
 
     def close_socket(self):
         # Release UDP socket
-        pass
+        self.socket.close()
